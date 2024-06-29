d88170dd0dfc67a0fc437218155d07656ae14994
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-06-29 15:51:25 +0900

    [level 0] Title: 소인수분해, Time: 0.22 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = set()
+    for i in range(2, n+1):
+        while n%i == 0:
+            answer.add(i)
+            n//=i
+    answer = list(answer)
+    return sorted(answer)
