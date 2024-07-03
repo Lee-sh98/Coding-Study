cc34add90b8afd2df4b2cc8a34fcaa6580458d2b
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-03 21:40:20 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 68 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,26 +1,14 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-
-class Person:
-    def __init__(self, n):
-        self.number = n+1
-    def __repr__(self):
-        return str(self.number)
 
 N, K = map(int, input().split())
 
 answer = []
-people = list(range(1, N+1))
-cur = 0
+q = deque(map(str, range(1, N+1)))
 
-while people:
-    count = K
-    while count:
-        count -= 1
-        if count == 0:
-            who = people.pop(cur)
-            answer.append(str(who))
-            break
-        cur = (cur+1)%len(people)
-        
+while q:
+    q.rotate(-K)
+    answer.append(q.pop())
+    
 print(f"<{', '.join(answer)}>")
