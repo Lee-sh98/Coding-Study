4a6bb502584e4c453780d43baadd4b09735ca226
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-09 18:50:53 +0900

    [Silver III] Title: N과 M (2), Time: 56 ms, Memory: 31588 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,16 @@
+import copy
+
+N, M = map(int, input().split())
+
+def dfs(arr, acc, used, m):
+    if not m:
+        print(*acc, sep=" ")
+    for i in arr:
+        if used[i] or (acc and acc[-1]>= i):
+            continue
+        next_used = copy.deepcopy(used)
+        next_used[i] = 1
+        dfs(arr, acc+[i], next_used, m-1)
+        
+arr = list(range(1, N+1))
+dfs(arr, [], [0]*(N+1),M)
