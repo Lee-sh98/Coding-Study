343eb0c2ad8b9817605c9238eef2a93df62389b9
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-10 18:39:39 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,4 +1,15 @@
 import sys
 
+def power(a, b, c):
+    res = 1
+    while b:
+        if b&1:
+            res*=a
+        a*=a
+        a = a%c
+        b>>=1
+    return res%c
 
-print(pow(*map(int, sys.stdin.readline().split())))
+A, B, C = map(int, sys.stdin.readline().split())
+
+print(power(A, B, C))
