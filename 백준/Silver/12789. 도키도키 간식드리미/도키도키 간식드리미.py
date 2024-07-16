26ebab814771ce764e7d9088dbeed3fb2327f366
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-16 15:23:23 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 60 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -4,26 +4,16 @@ input = sys.stdin.readline
 
 def solution(arr):
     stack = []
-    last = 0
-    while arr:
-        cur = arr.popleft()
-        while stack and stack[-1] == last+1:
-            last = stack.pop()
-        if cur == last + 1:
-            last = cur
-        else:
-            stack.append(cur)
+    target = 1
+    
+    for cur in arr:
+        stack.append(cur)
+        while stack and stack[-1] == target:
+            target = stack.pop() + 1
 
-    while stack and stack[-1] == last+1:
-        last = stack.pop()
-        
     return not stack
 
 N = int(input())
 arr = deque(map(int, input().split()))
 
-if solution(arr):
-    print("Nice")
-else:
-    print("Sad")
-
+print(("Sad", "Nice")[solution(arr)])
