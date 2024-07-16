c85c26d56ba74b52617392e8e639fc423fdd4815
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-16 22:24:02 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 656 ms, Memory: 80912 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -3,13 +3,16 @@ input = sys.stdin.readline
 
 S = input().rstrip()
 q = int(input())
-alpha = {i:[0]*(len(S)+1) for i in range(26)}
+alpha = {chr(i+ord('a')):[0] for i in range(26)}
 
 for i, s in enumerate(S):
-    for j in range(26):
-        alpha[j][i+1] = alpha[j][i] + int(ord(s)-ord('a')==j)
+    alpha[s] += [alpha[s][-1]]*(i-len(alpha[s])+1) + [alpha[s][-1]+1]
+
+for i in range(26):
+    c = chr(i+ord('a'))
+    alpha[c] += [alpha[c][-1]]*(len(S)-len(alpha[c])+1)
+
 for _ in range(q):
     a, l, r = input().split()
-    a_num = ord(a) - ord('a')
     l_num, r_num = int(l), int(r)
-    print(alpha[a_num][r_num+1] - alpha[a_num][l_num])
+    print(alpha[a][r_num+1] - alpha[a][l_num])
