38881d326195049626de982836e54fccef4d5f7b
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-17 05:37:34 +0900

    [Silver I] Title: IOIOI, Time: 356 ms, Memory: 35720 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,14 +1,22 @@
 import sys
+import re
 input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
 S = input().rstrip()
 
-PN = "I"+"OI"*N
-result = 0
-for i in range(len(S)-2*N+1):
-    cur = S[i:i+2*N+1]
-    if cur==PN:
-        result += 1
+result, count = 0, 0
+i = 0
+while i<M:
+    if S[i:i+3] == "IOI":
+        count += 1
+        i += 2
+        if count == N:
+            result += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
+
 print(result)
