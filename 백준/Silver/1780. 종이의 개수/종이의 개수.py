774db9e996ef1ae7e3f9fcab805f9d5cb509d61e
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-17 03:55:50 +0900

    [Silver II] Title: 종이의 개수, Time: 5788 ms, Memory: 75072 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -4,26 +4,29 @@ input = sys.stdin.readline
 
 def solution(arr):
     n = len(arr)
+    if n==1:
+        count[arr[0][0]]+=1
+        return
     total = set()
 
     for line in arr:
         total |= set(line)
 
     if len(total) == 1:
-        count[list(total)[0]+1] += 1
-        return
-
-    for i in range(0, n, n//3):
-        for j in range(0, n, n//3):
-            tmp = []
-            for k in range(i, i+n//3):
-                tmp.append(arr[k][j:j+n//3])
-            solution(tmp)
+        count[list(total)[0]] += 1
+    else:
+        for i in range(0, n, n//3):
+            for j in range(0, n, n//3):
+                tmp = []
+                for k in range(i, i+n//3):
+                    tmp.append(arr[k][j:j+n//3])
+                solution(tmp)
 
 N = int(input())
-count = [0, 0, 0]
+count = defaultdict(int)
 paper = [list(map(int, input().split())) for _ in range(N)]
 
 solution(paper)
 
-print(*count, sep="\n")
+for i in range(-1, 2):
+    print(count[i])
