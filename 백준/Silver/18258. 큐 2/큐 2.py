da620d32959f76c84a6685b0696e1a9f1203aa06
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-16 14:54:04 +0900

    [Silver IV] Title: 큐 2, Time: 1500 ms, Memory: 362860 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,11 +1,17 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+
+class Node:
+    def __init__(self, *v):
+        self.v, = v
+        self.next = None
 
 class Queue:
     def __init__(self):
-        self.q = deque([])
-        self.operation = {"push": self.q.append,
+        self.head = None
+        self.tail = None
+        self.length = 0
+        self.operation = {"push": self.append,
                           "pop": self.pop,
                           "size": self.size,
                           "empty": self.empty,
@@ -13,33 +19,55 @@ class Queue:
                           "back": self.back}
 
     def action(self, commend, *operand):
-        self.operation[commend](*operand)
+        result = self.operation[commend](*operand)
+        if commend != "push":
+            print(result)
+        
 
-    def print(self, func, otherwise=-1):
-        if self.q:
-            print(func())
+    def append(self, node):
+        if self.head == None:
+            self.head = node
+            self.tail = node
         else:
-            print(otherwise)
-
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+        
     def pop(self):
-        self.print(lambda: self.q.popleft())
+        if self.head == None:
+            return -1
+
+        result = self.head.v
+        self.head = self.head.next
+
+        if self.head == None:
+            self.tail = None
+        self.length -= 1
+        return result
 
     def size(self):
-        self.print(self.q.__len__, 0)
+        return self.length
 
     def empty(self):
-        self.print(lambda: 0, 1)
+        return int(self.length==0)
 
     def front(self):
-        self.print(lambda: self.q[0])
+        if self.head == None:
+            return -1
+        else:
+            return self.head.v
 
     def back(self):
-        self.print(lambda: self.q[-1])
+        if self.head == None:
+            return -1
+        else:
+            return self.tail.v
 
 N = int(input())
 q = Queue()
 
 for _ in range(N):
     commend, *operand = input().split()
-    q.action(commend, *operand)
+    node = [Node(*operand)] if operand else []
+    q.action(commend, *node)
     
