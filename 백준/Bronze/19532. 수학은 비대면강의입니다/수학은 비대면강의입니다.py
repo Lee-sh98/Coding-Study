d103f9163be9c3cb701e84ff76c5ee29622b2468
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-17 01:13:30 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,23 +1,8 @@
 import sys
 
-class Equation:
-    def __init__(self, p, q, r):
-        self.p = p
-        self.q = q
-        self.r = r
-        
-    def confirm(self, x, y):
-        return self.p*x+self.q*y == self.r
-
 a, b, c, d, e, f = map(int, sys.stdin.readline().split())
 
-e1, e2 = Equation(a,b,c), Equation(d,e,f)
-flag = False
-for x in range(-999, 1000):
-    for y in range(-999, 1000):
-        if e1.confirm(x, y) and e2.confirm(x, y):
-            print(x, y)
-            flag = True
-            break
-    if flag:
-        break
+x = (c*e-b*f)//(a*e-b*d)
+y = (a*f-c*d)//(a*e-b*d)
+
+print(x,y)
