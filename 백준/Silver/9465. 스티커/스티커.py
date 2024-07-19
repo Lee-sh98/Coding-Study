e10b8f7260453cf7342c173e7f37a93c8ef85384
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-20 04:33:11 +0900

    [Silver I] Title: 스티커, Time: 760 ms, Memory: 40892 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -3,14 +3,13 @@ input = sys.stdin.readline
 
 def solution():
     n = int(input())
-    sticker = list(map(list, zip(*(map(int, input().split()) for _ in range(2)))))
-    
-    dp = [[0, 0] for _ in range(n+2)]
+    sticker = list(list(map(int, input().split())) for _ in range(2))
+    dp = [[0]*(n+2) for _ in range(2)]
 
     for i in range(n):
         for j in range(2):
-            dp[i+2][j] = max(dp[i+1][j-1], dp[i][j-1]) + sticker[i][j]
-    return max(dp[-1])
+            dp[j][i+2] = max(dp[j-1][i+1], dp[j-1][i]) + sticker[j][i]
+    return max(dp[0][-1], dp[1][-1])
 
 
 T = int(input())
