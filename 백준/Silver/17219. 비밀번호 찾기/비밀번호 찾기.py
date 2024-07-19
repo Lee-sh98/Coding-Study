3f04d0929b131c729be19055f3db10117784c4d8
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-20 01:17:36 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 204 ms, Memory: 51552 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -4,5 +4,4 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 password = dict(input().split() for _ in range(N))
 
-for _ in range(M):
-    print(password.get(input().rstrip()))
+print(*(password.get(input().rstrip()) for _ in range(M)),sep="\n")
