effb2672175f562c7830796276437158af4d43bc
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-20 00:45:34 +0900

    [Silver III] Title: 포도주 시음, Time: 352 ms, Memory: 66388 KB -BaekjoonHub

--- 백준/Silver/31589. 포도주 시음/포도주 시음.py
+++ 백준/Silver/31589. 포도주 시음/포도주 시음.py
@@ -3,15 +3,20 @@ from collections import deque
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-T = deque(sorted(map(int, input().split())))
+T = list(sorted(map(int, input().split())))
 
-result = last = T.pop()
+result = last = T[-1]
 count = 1
+i, j = 0, N-2
+
 while count < K:
     if count%2:
-        last = T.popleft()
+        last = T[i]
+        i += 1
     else:
-        result += (picked:=T.pop()) - last
-        last = picked
+        result += T[j] - last
+        last = T[j]
+        j -= 1
     count += 1
+
 sys.stdout.write(str(result)+"\n")
