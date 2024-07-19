3bee6842d2a83cac2aa9b62c2c4bfb5341982001
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-19 20:35:32 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,11 +1,36 @@
 import sys
 input = sys.stdin.readline
 
+def count_sort(arr, reverse=False):
+    n = len(arr)
+    M = max(arr)
+
+    appearance = [0]*(M+1)
+    result = [0]*n
+    
+    for a in arr:
+        appearance[a] += 1
+    
+    i, count = 0, 0
+
+    while i <= M:
+        while appearance[i]:
+            result[count] = i
+            count += 1
+            appearance[i] -= 1
+        i += 1
+
+    return result if not reverse else result[::-1]
+    
+
 N = int(input())
 
-A = map(int, input().split())
-B = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
-S = sum(map(int.__mul__, sorted(A), sorted(B, reverse=True)))
-print(S)
+A_sorted = count_sort(A)
+B_sorted = count_sort(B, reverse=True)
 
+S = sum(map(int.__mul__, A_sorted, B_sorted))
+
+sys.stdout.write(str(S)+"\n")
