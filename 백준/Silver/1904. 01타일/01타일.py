993ba13e2e93583f18261953dda724affd42e3aa
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-22 20:17:28 +0900

    [Silver III] Title: 01타일, Time: 152 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,15 +1,14 @@
 import sys
 
 def tile(n):
-    dp = [0]*1000001
-    dp[1] = 1
-    dp[2] = 2
+    if n==1:
+        return 1
+    a, b = 1, 2
 
     for i in range(3, n+1):
-        dp[i] = (dp[i-1]+dp[i-2])%15746
-    return dp[n]
+        a, b = b%15746, (a+b)%15746
+    return b
     
-
 N = int(sys.stdin.readline())
 
 print(tile(N))
