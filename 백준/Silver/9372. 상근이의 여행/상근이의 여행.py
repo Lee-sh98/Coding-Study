edd4b9bdcb5bb7c076f83f1f928e2a84ab52102c
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-07-26 01:55:10 +0900

    [Silver IV] Title: 상근이의 여행, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,14 @@
+# 9372 Sanguen's travel
+import sys
+input = sys.stdin.readline
+
+def solution():
+    N, M = map(int, input().split())
+    for _ in range(M):
+        a, b = map(int, input().split())
+    print(N-1)
+    
+T = int(input())
+
+for _ in range(T):
+    solution()
