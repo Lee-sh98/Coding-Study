c3cccfb6cb37c16e5bd64d75f7fd7ff92c3c3734
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-08-13 13:08:56 +0900

    [Silver I] Title: 스타트링크, Time: 732 ms, Memory: 70992 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,31 +1,20 @@
 import sys
 from collections import deque
 
-def up(cur):
-    return cur + U
-
-def down(cur):
-    return cur - D
-
-def valid(cur):
-    return 1<=cur<=F
-
-F, S, G, U, D = map(int, input().split())
+F, S, G, U, D = map(int, sys.stdin.readline().split())
 visited = [0]*(F+1)
 result = "use the stairs"
-actions = [up, down]
 
-q = deque([(S, 0)])
+q = deque([S])
+visited[S] = 1
 while q:
-    cur, pushed = q.popleft()
-    visited[cur] = 1
+    cur = q.popleft()
     if cur == G:
-        result = pushed
+        result = visited[cur] - 1
         break
-    for action in actions:
-        next_floor = action(cur)
-        if valid(next_floor) and not visited[next_floor]:
-            visited[next_floor] = 1
-            q.append((next_floor, pushed+1))
+    for next_floor in [cur+U, cur-D]:
+        if 0<next_floor<=F and not visited[next_floor]:
+            q.append(next_floor)
+            visited[next_floor] = visited[cur] + 1
 
 print(result)
