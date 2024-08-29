b389ca294f76fb8fd746131d3a5840f32e562976
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-08-30 00:52:36 +0900

    [Gold IV] Title: 단어 수학, Time: 56 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.py
+++ 백준/Gold/1339. 단어 수학/단어 수학.py
@@ -8,9 +8,9 @@ total = defaultdict(int)
 result = 0
 
 for a in arr:
-    for idx, v in enumerate(a[::-1]):
-        total[v]+=10**idx
+    for i, v in enumerate(a[::-1]):
+        total[v] += 10**i
 
-for i, (k, v) in enumerate(sorted(total.items(), key=lambda x: x[1], reverse=True)):
+for i, v in enumerate(sorted(total.values(), reverse=True)):
     result += v*(9-i)
 print(result)
