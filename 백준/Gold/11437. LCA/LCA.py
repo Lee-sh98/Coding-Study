c39f3e5970573b298adfa17cdc7a40bc5c4bd097
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-09-03 02:43:57 +0900

    [Gold III] Title: LCA, Time: 760 ms, Memory: 73636 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.py
+++ 백준/Gold/11437. LCA/LCA.py
@@ -1,17 +1,26 @@
 import sys
 from collections import deque
+sys.setrecursionlimit(200000)
 input = sys.stdin.readline
+
+def memoize(func):
+    memo = {}
+    def helper(a, b):
+        if (a, b) not in memo:
+            memo[a, b] = func(a, b)
+        return memo[a, b]
+    return helper
 
 def lca(a, b):
     if d[a] > d[b]:
-        a, b = b, a
-    while d[a] != d[b]:
-        b = parent[b]
-
+        return lca(parent[a], b)
+    elif d[a] < d[b]:
+        return lca(a, parent[b])
     while a!=b:
         a = parent[a]
         b = parent[b]
     return a
+lca = memoize(lca)
 
 N = int(input())
 parent = [0]*(N+1)
