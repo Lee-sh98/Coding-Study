6102778c918fbc5ac1af687c53bb27e862f61514
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-09-10 01:16:24 +0900

    [Silver IV] Title: 30번, Time: 180 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13116. 30번/30번.py
+++ 백준/Silver/13116. 30번/30번.py
@@ -1,5 +1,4 @@
 import sys
-from math import log2
 input = sys.stdin.readline
 
 
@@ -7,13 +6,9 @@ T = int(input())
 for _ in range(T):
     A, B = map(int, input().split())
 
-    while int(log2(A)) != int(log2(B)):
-        if log2(A) > log2(B):
-            A >>= 1
-        else:
-            B >>= 1
-
     while A != B:
-        A >>= 1
-        B >>= 1
+        if A>B:
+            A//=2
+        elif B>A:
+            B//=2
     print(A*10)
