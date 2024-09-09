60dc8d5f80aaaa962ac1655f2ed85de4acb3e602
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-09-10 03:49:24 +0900

    [Silver III] Title: 콜라츠 추측, Time: 104 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6615. 콜라츠 추측/콜라츠 추측.py
+++ 백준/Silver/6615. 콜라츠 추측/콜라츠 추측.py
@@ -1,39 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-def memoize(func):
-    memo = {}
-
-    def helper(n):
-        if n not in memo:
-            memo[n] = func(n)
-        return memo[n]
-    return helper
-
-def get_ancestor(n):
-    if n == 1:
-        return [1]
-    if n%2:
-        return [n]+get_ancestor(3*n+1)
-    else:
-        return [n]+get_ancestor(n//2)
-
-get_ancestor = memoize(get_ancestor)
+def collatz(n):
+    yield n
+    while n!=1:
+        q, r = divmod(n, 2)
+        if r:
+            yield (n:=3*n+1)
+        else:
+            yield (n:=q)
 
 while True:
     A, B = map(int, input().split())
     if A==B==0:
         break
 
-    pA = get_ancestor(A)
-    pB = get_ancestor(B)
+    pA = list(collatz(A))
+    pB = list(collatz(B))
 
-    common = 0
-    for a, b in zip(pA[::-1], pB[::-1]):
-        if a==b:
-            common += 1
-        else:
-            break
-    SA, SB = len(pA)-common, len(pB)-common
-    C = pA[-common]
+    SA, SB = len(set(pA)-set(pB)), len(set(pB)-set(pA))
+    C = pA[SA]
     print(f"{A} needs {SA} steps, {B} needs {SB} steps, they meet at {C}")
