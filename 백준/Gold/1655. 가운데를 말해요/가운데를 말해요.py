8903aa115efd302d2e7ad4b3a94064b89c9cf55f
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-09-17 16:06:31 +0900

    [Gold II] Title: 가운데를 말해요, Time: 236 ms, Memory: 37132 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -0,0 +1,22 @@
+import sys
+from heapq import *
+input = sys.stdin.readline
+
+N = int(input())
+lq, rq = [], [] # max heap and min heap
+
+for _ in range(N):
+    num = int(input())
+
+    if len(lq) == len(rq):
+        heappush(lq, -num)
+    else:
+        heappush(rq, num)
+
+    if rq and -lq[0]>rq[0]:
+        lv, rv = heappop(lq), heappop(rq)
+
+        heappush(lq, -rv)
+        heappush(rq, -lv)
+
+    print(-lq[0])
