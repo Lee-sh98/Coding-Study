fc55432628add4740a4461ef3d41f658a7756fb9
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-09-22 21:53:45 +0900

    [Gold V] Title: 배열 탈출, Time: 6988 ms, Memory: 268432 KB -BaekjoonHub

---
+++ 백준/Gold/11909. 배열 탈출/배열 탈출.py
@@ -0,0 +1,22 @@
+import sys
+from heapq import *
+input = sys.stdin.readline
+INF = int(1e9)
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0]*n for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if i==j==0:
+            continue
+        c1, c2 = INF, INF
+        if j!=0:
+            c1 = dp[i][j-1] + max(0, arr[i][j]-arr[i][j-1]+1)
+        if i!=0:
+            c2 = dp[i-1][j] + max(0, arr[i][j]-arr[i-1][j]+1)
+
+        dp[i][j] += min(c1, c2)
+
+print(dp[n-1][n-1])
