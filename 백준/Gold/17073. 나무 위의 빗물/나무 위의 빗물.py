b9fb11390f4ea13257072613b37be22231e4808b
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-09-26 20:20:40 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 508 ms, Memory: 35024 KB -BaekjoonHub

--- 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
@@ -1,33 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-def make_tree():
-    children = [[] for _ in range(N+1)]
-    stack = [1]
-    while stack:
-        cur = stack.pop()
-
-        for node in edges[cur]:
-            if not children[node]:
-                children[cur].append(node)
-                stack.append(node)
-    return children
-
 N, W = map(int, input().split())
-water = [0]*(N+1)
-water[1] = W
-edges = [[] for _ in range(N+1)]
+edges = [0]*(N+1)
+result = 0
 
 for _ in range(N-1):
     U, V = map(int, input().split())
-    edges[U].append(V)
-    edges[V].append(U)
+    edges[U] += 1
+    edges[V] += 1
 
-children = make_tree()
+for i in range(2, N+1):
+    if edges[i] == 1:
+        result += 1
 
-count = 0
-for i in range(1, N+1):
-    if not children[i]:
-        count += 1
-
-print(W/count)
+print(W/result)
