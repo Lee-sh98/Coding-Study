3c250a60fcec6d62f403911ac5bda591b77c7b10
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-09-26 20:14:42 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 2180 ms, Memory: 170144 KB -BaekjoonHub

--- 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 def make_tree():
@@ -19,27 +18,16 @@ water = [0]*(N+1)
 water[1] = W
 edges = [[] for _ in range(N+1)]
 
-
 for _ in range(N-1):
     U, V = map(int, input().split())
     edges[U].append(V)
     edges[V].append(U)
 
 children = make_tree()
-q = deque([1])
-while q:
-    cur = q.popleft()
-    if not children[cur]:
-        continue
-    each_water = water[cur]/len(children[cur])
-    water[cur] = 0
-    for node in children[cur]:
-        water[node] += each_water
-        q.append(node)
 
 count = 0
 for i in range(1, N+1):
-    if water[i]:
+    if not children[i]:
         count += 1
 
 print(W/count)
