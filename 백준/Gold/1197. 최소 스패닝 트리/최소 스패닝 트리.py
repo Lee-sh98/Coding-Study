1327ff6e5a0569fd0e86fa129daf54c338f0a7fa
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-09-29 00:14:41 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 328 ms, Memory: 58548 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -3,28 +3,28 @@ from heapq import *
 input = sys.stdin.readline
 
 V, E = map(int, input().split())
-nodes = [0]*(V+1)
+visited = [0]*(V+1)
 edges = [[] for _ in range(V+1)]
+
 for _ in range(E):
     A, B, C = map(int, input().split())
     heappush(edges[A], (C, B))
     heappush(edges[B], (C, A))
 
-
-cur = 1
-nodes[1] = 1
-q = edges[1]
-count = 1
+q = [(0, 1)]
+count = 0
 result = 0
 while count < V:
-    while True:
-        weight, node = heappop(q)
-        if not nodes[node]:
-            nodes[node] = 1
-            result += weight
-            count += 1
-            for edge in edges[node]:
-                heappush(q, edge)
-            break
+    cost, cur = heappop(q)
+    if visited[cur]:
+        continue
+    visited[cur] = 1
+    result += cost
+    count += 1
+
+    for edge in edges[cur]:
+        if visited[edge[1]]:
+            continue
+        heappush(q, edge)
 
 print(result)
