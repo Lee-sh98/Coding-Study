af5fae8189f6b2878f9a86e7afb138a544e1f6b2
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-09-29 00:43:57 +0900

    [Gold V] Title: 하노이 탑, Time: 728 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,14 @@
+import sys
+
+def hanoi(n, start, mid, end):
+    if n==1:
+        print(start, end)
+        return
+    hanoi(n-1, start, end, mid)
+    print(start, end)
+    hanoi(n-1, mid, start, end)
+
+N = int(sys.stdin.readline())
+print(2**N-1)
+if N <= 20:
+    hanoi(N, 1, 2, 3)
