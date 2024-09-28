51c36ff6897d9fd05c9ff8742c82934e151392c6
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-09-29 01:51:22 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 148 ms, Memory: 53860 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -1,23 +1,21 @@
 import sys
 
-def supply1(prev, cover):
+def supply1(prev, n):
+    cover = [" "*n, " "*n]
     for p in prev:
         yield p.join(cover)
 
 def supply2(prev):
-    for p in zip(prev, prev):
-        yield " ".join(p)
+    yield from map(" ".join, zip(prev, prev))
 
 def solve(n):
     if n==3:
         return ["  *  ", " * * ", "*****"]
 
-    n >>= 1
+    n //= 2
     prev = solve(n)
-    result = []
-    cover = [" "*n, " "*n]
     
-    r1 = list(supply1(prev, cover))
+    r1 = list(supply1(prev, n))
     r2 = list(supply2(prev))
     
     return r1+r2
