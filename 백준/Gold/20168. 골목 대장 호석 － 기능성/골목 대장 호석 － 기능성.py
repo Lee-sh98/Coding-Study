7edbe1ac5b7e814b9086f4527025171894310331
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-03 14:50:38 +0900

    [Gold V] Title: 골목 대장 호석 - 기능성, Time: 124 ms, Memory: 31120 KB, Score: 18 point -BaekjoonHub

--- 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.py
+++ 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.py
@@ -1,35 +1,33 @@
 import sys
-from heapq import *
 input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
 INF = int(1e10)
 
-def dijkstra(start, end):
+def solve(cur, total_cost, max_cost):
     global result
-    distances = [INF]*(N+1)
-    distances[start] = 0
-
-    q = [(0, 0, start)]
-    while q:
-        max_cost, total_cost, cur = heappop(q)
-        if distances[cur] < total_cost:
-            continue
-        if cur==end:
-            result = min(result, max_cost)
-        for node, next_cost in edges[cur]:
-            n_max = max(max_cost, next_cost)
-            n_cost = total_cost+next_cost
-            if n_cost<distances[node] and n_cost<=C:
-                distances[node] = n_cost
-                heappush(q, (n_max, n_cost, node))
-    
+    if total_cost > C:
+        return
+    if cur == B:
+        result = min(result, max_cost)
+        return
+    for node, cost in edges[cur]:
+        if not visited[node]:
+            visited[node] = True
+            solve(node, total_cost+cost, max(max_cost, cost))
+            visited[node] = False
+    return
 
 N, M, A, B, C = map(int, input().split())
 edges = [[] for _ in range(N+1)]
+visited = [False]*(N+1)
+visited[A] = True
 result = INF
+
 for _ in range(M):
     u, v, l = map(int, input().split())
     edges[u].append((v, l))
     edges[v].append((u, l))
-dijkstra(A, B)
-print((result, -1)[result==INF])
+
+solve(A, 0, 0)
+if result == INF:
+    result = -1
+print(result)
