4ab8cd977ee100bef211891969b7970a798e5e39
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-03 18:24:05 +0900

    [Gold V] Title: 강의실 배정, Time: 404 ms, Memory: 86516 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -7,9 +7,9 @@ lectures = [list(map(int, input().split())) for _ in range(N)]
 lectures.sort(key=lambda x: (x[0], x[1]))
 using = [lectures[0][1]]
 
-for start, end in lectures[1:]:
-    if using[0] <= start:
+for i in range(1, N):
+    if using[0] <= lectures[i][0]:
         heappop(using)
-    heappush(using, end)
+    heappush(using, lectures[i][1])
 
 print(len(using))
