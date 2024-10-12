e95f666cab9064bac3dd031574161588fadb291e
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-12 21:09:48 +0900

    [Gold IV] Title: 스도쿠, Time: 628 ms, Memory: 114880 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -2,29 +2,22 @@ import sys
 input = sys.stdin.readline
 
 arr = [list(map(int, input().split())) for _ in range(9)]
+rows = [[False]*9 for _ in range(9)]
+cols = [[False]*9 for _ in range(9)]
+rects = [[False]*9 for _ in range(9)]
 blanks = []
 
 for i in range(9):
     for j in range(9):
         if arr[i][j] == 0:
             blanks.append((i, j))
+        else:
+            rows[i][arr[i][j]-1] = True
+            cols[j][arr[i][j]-1] = True
+            rects[(i//3*3)+(j//3)][arr[i][j]-1] = True
 
-def checkRow(x, a):
-    return a not in arr[x]
-
-def checkCol(y, a):
-    for j in range(9):
-        if a == arr[j][y]:
-            return False
-    return True
-
-def checkRect(x, y, a):
-    nx, ny = x//3*3, y//3*3
-    for i in range(nx, nx+3):
-        for j in range(ny, ny+3):
-            if a==arr[i][j]:
-                return False
-    return True
+def check(x, y, a):
+    return not(rows[x][a]) and not(cols[y][a]) and not(rects[(x//3*3)+(y//3)][a])
 
 def dfs(idx):
     if idx == len(blanks):
@@ -34,10 +27,16 @@ def dfs(idx):
 
     x, y = blanks[idx]
     
-    for i in range(1, 10):
-        if checkRow(x, i) and checkCol(y, i) and checkRect(x, y, i):
-            arr[x][y] = i
+    for i in range(9):
+        if check(x, y, i):
+            arr[x][y] = i+1
+            rows[x][i] = True
+            cols[y][i] = True
+            rects[(x//3*3)+(y//3)][i] = True
             dfs(idx+1)
             arr[x][y] = 0
+            rows[x][i] = False
+            cols[y][i] = False
+            rects[(x//3*3)+(y//3)][i] = False
 
 dfs(0)
