804ac039df07cd6f841b3c4816f5a74d90762e0c
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-14 19:58:27 +0900

    [Gold IV] Title: 알파벳, Time: 4360 ms, Memory: 275716 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -3,24 +3,22 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**5)
 A = ord('A')
 
-def solve(x, y):
-    result = 0
+def solve(x, y, mask, count):
+    global result
+    result = max(result, count)
     for dx, dy in directions:
         nx, ny = x+dx, y+dy
         if not(0<=nx<R and 0<=ny<C):
             continue
-        idx = ord(arr[nx][ny])-A
-        if not used[idx]:
-            used[idx] = True
-            result = max(result, solve(nx, ny))
-            used[idx] = False
-    return result+1
+        
+        nmask = 1 << ord(arr[nx][ny]) - A
+        if not mask & nmask:
+            solve(nx, ny, mask|nmask, count+1)
 
 R, C = map(int, input().split())
 arr = [list(input().rstrip()) for _ in range(R)]
 directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-used = [False]*26
-used[ord(arr[0][0]) - A] = True
+result = 0
 
-result = solve(0, 0)
+solve(0, 0, 1<<ord(arr[0][0])-A, 1)
 print(result)
