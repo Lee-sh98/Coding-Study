6c09b198d90aca002630f6887765bef9ee537e04
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-16 00:34:57 +0900

    [Silver V] Title: 크리 문자열, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11059. 크리 문자열/크리 문자열.py
+++ 백준/Silver/11059. 크리 문자열/크리 문자열.py
@@ -8,15 +8,13 @@ for i in range(L):
     acc[i+1] = acc[i] + int(S[i])
 
 result = 0
-found = False
 for size in range(L//2, 0, -1):
     for i in range(L-2*size+1):
         if (acc[i+size]-acc[i]) == (acc[i+2*size]-acc[i+size]):
-            found = True
-            result = size*2
+            result = 2*size
             break
 
-    if found:
+    if result:
         break
 
 print(result)
