774fa1e1df04a7b76cfd98f7e08e76a453b2fad0
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-18 01:31:23 +0900

    [Gold II] Title: 등산, Time: 44 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/1486. 등산/등산.py
+++ 백준/Gold/1486. 등산/등산.py
@@ -14,35 +14,44 @@ def input_line():
             result.append(c - a + 26)
     return result
 
-N, M, T, D = map(int, input().split())
-graph = [input_line() for _ in range(N)]
-directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+def dijkstra(sx, sy, compare):
+    distance = [[INF]*M for _ in range(N)]
+    distance[sx][sy] = 0
 
-dp = [[[[INF]*M for _ in range(N)] for _ in range(M)] for _ in range(N)]
-for x in range(N):
-    for y in range(M):
-        dp[x][y][x][y] = 0
+    q = [(0, sx, sy)]
+    while q:
+        dist, x, y = heappop(q)
+        if distance[x][y] < dist:
+            continue
         for dx, dy in directions:
             nx, ny = x+dx, y+dy
             if not (0<=nx<N and 0<=ny<M):
                 continue
-            dh = graph[x][y] - graph[nx][ny]
+
+            dh = graph[x][y]-graph[nx][ny]
             if abs(dh) > T:
                 continue
-            dp[x][y][nx][ny] = min(dp[x][y][nx][ny], 1 if dh >= 0 else dh**2)
 
-for i in range(N):
-    for j in range(M):
-        for u in range(N):
-            for v in range(M):
-                for x in range(N):
-                    for y in range(M):
-                        dp[u][v][x][y] = min(dp[u][v][x][y], dp[u][v][i][j]+dp[i][j][x][y])
-MAX = 0
-for x in range(N):
-    for y in range(M):
-        cost = dp[0][0][x][y] + dp[x][y][0][0]
-        if cost < INF and cost <= D:
-            MAX = max(MAX, graph[x][y])
+            cost = dist + (1 if compare(dh) else dh**2)
+            if cost < distance[nx][ny]:
+                distance[nx][ny] = cost
+                heappush(q, (cost, nx, ny))
+    return distance
 
-print(MAX)
+def solve(sx, sy):
+    ascend = dijkstra(0, 0, int(0).__le__)
+    descend = dijkstra(0, 0, int(0).__ge__)
+    
+    MAX = 0
+    for i in range(N):
+        for j in range(M):
+            if ascend[i][j]+descend[i][j]<=D and MAX < graph[i][j]:
+                MAX = graph[i][j]
+    return MAX
+
+N, M, T, D = map(int, input().split())
+graph = [input_line() for _ in range(N)]
+directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+result = solve(0, 0)
+print(result)
