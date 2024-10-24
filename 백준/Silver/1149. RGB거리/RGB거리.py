4d192c829bdf498ae87e224c5140a85ea976a69b
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-25 00:42:30 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -4,11 +4,11 @@ input = sys.stdin.readline
 N = int(input())
 cost = [list(map(int, input().split())) for _ in range(N)]
 
-dp = [[0]*3]
+dp = [0]*3
+tmp = [0]*3
 for c in cost:
-    tmp = []
     for i in range(3):
-        tmp.append(c[i] + min(dp[-1][:i]+dp[-1][i+1:]))
-    dp.append(tmp)
+        tmp[i] = c[i] + min(dp[:i]+dp[i+1:])
+    dp = tmp[:]
 
-print(min(dp[-1]))
+print(min(dp))
