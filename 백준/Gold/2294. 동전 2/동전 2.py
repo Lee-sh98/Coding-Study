d4d505686b3ca0f9276612f9bb4d2ba200c27322
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-26 01:46:19 +0900

    [Gold V] Title: 동전 2, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -5,10 +5,9 @@ INF = int(1e9)
 n, k = map(int, input().split())
 coins = [int(input()) for _ in range(n)]
 coins.sort()
-dp = [INF]*(k+1)
-dp[0] = 0
+dp = [0]*(k+1)
 
 for i in range(1, k+1):
-    dp[i] = min(dp[i], min((dp[i-coin] for coin in coins if coin<=i), default=INF)+1)
+    dp[i] = min((dp[i-coin] for coin in coins if coin<=i), default=INF)+1
 
-print((dp[k], -1)[dp[k]==INF])
+print((dp[k], -1)[dp[k]>=INF])
