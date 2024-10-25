41fe3e2375434e3ebd0b251ff32f1ee982786220
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-26 02:19:23 +0900

    [Gold V] Title: 합분해, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,11 +1,9 @@
 import sys
-sys.setrecursionlimit(10**5)
 MOD = 1_000_000_000
                 
 N, K = map(int, sys.stdin.readline().split())
+a = b = 1
 
-a = 1
-b = 1
 for i in range(1, K):
     a *= N+i
     b *= i
