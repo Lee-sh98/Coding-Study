8dd8c25d6b1288d082098b298f5df4f8a9f17623
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-25 23:41:24 +0900

    [Gold IV] Title: 동전 1, Time: 184 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -4,9 +4,10 @@ input = sys.stdin.readline
 n, k = map(int, input().split())
 coins = [int(input()) for _ in range(n)]
 dp = [0]*(k+1)
-dp[0] = 1
 
 for coin in coins:
+    if coin <= k:
+        dp[coin] += 1
     for i in range(coin, k+1):
         dp[i] += dp[i-coin]
 
