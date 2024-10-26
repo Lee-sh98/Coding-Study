f34be9de0ac3a475027c62d1b11c63146e691a86
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-26 15:26:05 +0900

    [Silver I] Title: 팔, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1105. 팔/팔.py
+++ 백준/Silver/1105. 팔/팔.py
@@ -1,15 +1,12 @@
 import sys
 
-L, R = input().split()
-
-if len(L) != len(R):
-    print(0)
-else:
-    c = 0
+L, R = sys.stdin.readline().split()
+c = 0
+if len(L) == len(R):
     for l, r in zip(L, R):
         if l!=r:
             break
         if l==r=='8':
             c += 1
 
-    print(c)
+print(c)
