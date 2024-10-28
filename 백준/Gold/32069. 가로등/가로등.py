ad90f1923fd192559a0bdb7cb9fc921e33c9b346
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-29 00:58:13 +0900

    [Gold V] Title: 가로등, Time: 4712 ms, Memory: 289856 KB, Score: 40 point -BaekjoonHub

--- 백준/Gold/32069. 가로등/가로등.py
+++ 백준/Gold/32069. 가로등/가로등.py
@@ -1,24 +1,23 @@
 import sys
 from collections import deque
-from heapq import *
 input = lambda: map(int, sys.stdin.readline().split())
 
 L, N, K = input()
 lights = list(input())
-street = [L]*(L+1)
+street = [-1]*(L+1)
 
-q = [(0, light) for light in lights]
+q = deque((0, light) for light in lights)
 for l in lights:
     street[l] = 0
 
 while q:
-    brightness, cur = heappop(q)
-    if street[cur] < brightness:
-        continue
+    brightness, cur = q.popleft()
+    
     for d in [-1, 1]:
         node = cur+d
-        if 0<=node<=L and brightness+1 < street[node]:
+        
+        if 0<=node<=L and street[node]==-1:
             street[node] = brightness+1
-            heappush(q, (brightness+1, node))
+            q.append((brightness+1, node))
 
 print("\n".join(map(str, sorted(street)[:K])))
