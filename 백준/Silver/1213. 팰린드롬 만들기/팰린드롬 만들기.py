6cdb2d27e650c4c656fd9c9f97424bb7e46faff8
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-28 16:59:16 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 60 ms, Memory: 34020 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -5,12 +5,11 @@ count = Counter(sys.stdin.readline().rstrip())
 if len(list(filter(int(2).__rmod__, count.values()))) > 1:
     print("I'm Sorry Hansoo")
 else:
-    left, right = "", ""
-    mid = ""
+    side, mid = "", ""
     s = list(sorted(count))
+    
     for ch in s:
         if count[ch]%2:
             mid = ch
-        left += ch*(count[ch]//2)
-        right = ch*(count[ch]//2) + right
-    print(left+mid+right)
+        side += ch*(count[ch]//2)
+    print(side+mid+side[::-1])
