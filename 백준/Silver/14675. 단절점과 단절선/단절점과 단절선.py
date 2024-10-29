18472c9783d3a155d4aae4544d9f4030f11b471a
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-30 01:03:16 +0900

    [Silver I] Title: 단절점과 단절선, Time: 224 ms, Memory: 31824 KB -BaekjoonHub

--- 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.py
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.py
@@ -5,11 +5,10 @@ N, = input()
 edges = [0]*(N+1)
 
 for _ in range(N-1):
-    u, v = input()
-    edges[u]+=1
-    edges[v]+=1
+    for i in input():
+        edges[i]+=1
 
 q, = input()
 for _ in range(q):
     t, k = input()
-    print(("no", "yes")[edges[k]>1 or t==2])
+    print(("yes", "no")[t==1 and edges[k]==1])
