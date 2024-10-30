a2d512d2e8ce215b11b771d62caa08d65b2677c9
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-30 14:08:10 +0900

    [Silver II] Title: 이동하기, Time: 660 ms, Memory: 70104 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -2,10 +2,9 @@ import sys
 input = lambda: map(int, sys.stdin.readline().split())
 
 N, M = input()
-graph = [list(input()) for _ in range(N)]
-dp = [[0]*(M+1) for _ in range(N+1)]
+graph = [[0]*(M+1)]+[[0]+list(input()) for _ in range(N)]
 
 for i in range(1, N+1):
     for j in range(1, M+1):
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1])+graph[i-1][j-1]
-print(dp[N][M])
+        graph[i][j] += max(graph[i][j-1], graph[i-1][j])
+print(graph[N][M])
