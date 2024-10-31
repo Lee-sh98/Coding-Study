352a5805cd84005b4e38be8f437d02bf960a1548
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-10-31 23:54:27 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1040 ms, Memory: 148924 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -1,7 +1,4 @@
 import sys
-from collections import Counter
 input = lambda:int(sys.stdin.readline())
 
-count = Counter(input() for _ in range(input()))
-for n in sorted(count, reverse=True):
-    print("\n".join([str(n)]*count[n]))
+print("\n".join(map(str, sorted((input() for _ in range(input())), reverse=True))))
