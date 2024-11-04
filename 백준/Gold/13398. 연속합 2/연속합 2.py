eb1a35211c75e9cae3bf461c4a4c8969b8fbc29c
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-04 18:57:57 +0900

    [Gold V] Title: 연속합 2, Time: 136 ms, Memory: 38632 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -3,13 +3,11 @@ input = sys.stdin.readline
 INF = -int(1e9)
 
 n = int(input())
-dp = [[INF]*3 for _ in range(n+1)]
+dp = [INF]*2
+result = INF
 
-dp = [[INF]*(n+1) for _ in range(3)]
-dp[0] = list(map(int, input().split()))+[0]
+for m in map(int, input().split()):
+    dp = [max(dp[0]+m, m), max(dp[1]+m, dp[0])]
+    result = max(result, *dp)
 
-for i in range(n):
-    for j in [1, 2]:
-        dp[j][i+1] = max(dp[j][i]+dp[0][i], dp[j-1][i])
-
-print(max(map(max, dp[1:])))
+print(result)
