633fef3a7dba9d8187e1132c6c0a3601b3d828a8
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-06 14:08:30 +0900

    [Gold V] Title: 1학년, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.py
+++ 백준/Gold/5557. 1학년/1학년.py
@@ -2,14 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-*arr, target = list(map(int, input().split()))
+*arr, target = map(int, input().split())
 dp = [[0]*21 for _ in range(N-1)]
 dp[0][arr[0]] = 1
 
-for i in range(1, N-1):
+for i, a in enumerate(arr[1:]):
     for j in range(21):
-        for k in [arr[i], -arr[i]]:
-            if 0<=j+k<=20:
-                dp[i][j]+=dp[i-1][j+k]
-
+        for k in filter(lambda x: 0<=j+x<=20, [a, -a]):
+            dp[i+1][j]+=dp[i][j+k]
+        
 print(dp[N-2][target])
