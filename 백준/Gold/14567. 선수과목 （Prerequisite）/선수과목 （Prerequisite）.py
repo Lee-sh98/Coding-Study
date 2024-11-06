5b090c194438557d0716214f9efa3210e27591fe
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-06 16:35:37 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 712 ms, Memory: 50292 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -2,18 +2,17 @@ import sys
 input = lambda: map(int, sys.stdin.readline().split())
 
 N, M = input()
-children = [[] for _ in range(N+1)]
-indegree = [0]*(N+1)
+children = [[] for _ in range(N)]
+stack = []
+answer = [0]*N
+indegree = [0]*N
 
 for _ in range(M):
     A, B = input()
-    children[A].append(B)
-    indegree[B] += 1
+    children[A-1].append(B-1)
+    indegree[B-1] += 1
 
-stack = []
-answer = [0]*(N+1)
-
-for i in range(1, N+1):
+for i in range(N):
     if indegree[i] == 0:
         stack.append(i)
         answer[i] = 1
@@ -27,4 +26,4 @@ while stack:
         if indegree[child] == 0:
             stack.append(child)
 
-print(" ".join(map(str, answer[1:])))
+print(" ".join(map(str, answer)))
