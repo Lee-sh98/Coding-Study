bd09b946f4db0cfaaac6918398c31448fbe181d8
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-08 00:15:06 +0900

    [Silver III] Title: 문자열 집합 조합하기, Time: 84 ms, Memory: 37464 KB -BaekjoonHub

--- 백준/Silver/25328. 문자열 집합 조합하기/문자열 집합 조합하기.py
+++ 백준/Silver/25328. 문자열 집합 조합하기/문자열 집합 조합하기.py
@@ -1,11 +1,8 @@
 from collections import Counter
-from itertools import combinations as t, chain
-
-*W, k = (input() for _ in range(4))
-
-c = Counter(chain(*map(lambda w: t(w, int(k)), W)))
-
-if (r:=sorted(filter(lambda d: c[d]>=2, c))):
-    print("\n".join(map("".join, r)))
+from itertools import combinations as t,chain
+*W,k=(input() for _ in range(4))
+c=Counter(chain(*map(lambda w:t(w,int(k)),W)))
+if(r:=sorted(filter(lambda d:c[d]>=2,c))):
+    print("\n".join(map("".join,r)))
 else:
     print(-1)
