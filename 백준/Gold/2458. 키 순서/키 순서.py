68999d923b21097b67f7abeaa374ac24da55176b
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-09 20:14:51 +0900

    [Gold IV] Title: 키 순서, Time: 288 ms, Memory: 116704 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.py
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -1,22 +1,15 @@
-import sys
-input = lambda: map(int, sys.stdin.readline().split())
-
-def dfs(s, c):
-    v[c][s] = v[s][c] = 1
-    
+f=lambda:map(int,input().split())
+def dfs(s,c):
+    v[c][s]=v[s][c]=1
     for t in e[c]:
         if not v[s][t]:
             dfs(s, t)
-
-N, M = input()
-v = [[0]*N for _ in range(N)]
-e = [[] for _ in range(N)]
-
+N,M=f()
+v=[[0]*N for _ in range(N)]
+e=[[]for _ in range(N)]
 for _ in range(M):
-    a, b = input()
+    a,b=f()
     e[a-1].append(b-1)
-
 for i in range(N):
-    dfs(i, i)
-
-print(sum(sum(v[i])==N for i in range(N)))
+    dfs(i,i)
+print(sum(map(all,v)))
