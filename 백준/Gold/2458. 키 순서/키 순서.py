aa8ca4e3a74e9a7284e2730ca24e9f664899637a
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-09 19:21:12 +0900

    [Gold IV] Title: 키 순서, Time: 1448 ms, Memory: 113496 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.py
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -4,19 +4,21 @@ INF = int(1e9)
 
 N, M = map(int, input().split())
 dp = [[0]*N for _ in range(N)]
+indegree = [0]*N
+outdegree = [0]*N
 
 for _ in range(M):
     a, b = map(int, input().split())
     dp[a-1][b-1] = 1
+    indegree[b-1] += 1
+    outdegree[a-1] += 1
 
 for k in range(N):
     for i in range(N):
         for j in range(N):
-            dp[i][j] = dp[i][j] or (dp[i][k] and dp[k][j])
+            if not dp[i][j] and dp[i][k] and dp[k][j]:
+                dp[i][j] = 1
+                indegree[j] += 1
+                outdegree[i] += 1
 
-tp = list(map(list, zip(*dp)))
-
-result = 0
-for i in range(N):
-    result += sum(dp[i])+sum(tp[i]) == N-1
-print(result)
+print(sum(map(lambda i: indegree[i]+outdegree[i] == N-1, range(N))))
