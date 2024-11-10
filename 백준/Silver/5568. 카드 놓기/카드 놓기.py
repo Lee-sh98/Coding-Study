ec2ffac0c6d35baff24f3e01f8f353f762701c34
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-10 21:58:28 +0900

    [Silver IV] Title: 카드 놓기, Time: 32 ms, Memory: 31632 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.py
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -1,19 +1,4 @@
-import sys
-input = sys.stdin.readline
-
-def solve(x, c):
-    if c==k:
-        result.add(x)
-    else:
-        for i in range(n):
-            if not used[i]:
-                used[i] = 1
-                solve(x+arr[i], c+1)
-                used[i] = 0
-
-n, k = [int(input()) for _ in range(2)]
-arr = [input().rstrip() for _ in range(n)]
-used = [0]*n
-result = set()
-solve("", 0)
-print(len(result))
+import itertools as i
+r=range
+n,k=[int(input())for _ in r(2)]
+print(len(set(map("".join,i.permutations((input()for _ in r(n)),k)))))
