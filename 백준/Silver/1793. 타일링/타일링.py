a932f232b89a2ab79e51c651a5d0eb9d1b55a66e
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-10 19:54:30 +0900

    [Silver II] Title: 타일링, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1793. 타일링/타일링.py
+++ 백준/Silver/1793. 타일링/타일링.py
@@ -1,12 +1,18 @@
 import sys
 
-dp = {0:1, 1:1}
-for i in range(2, 251):
-    dp[i] = dp[i-1]+2*dp[i-2]
+def solve(n):
+    global MAX
+    if n not in dp:
+        for i in range(MAX, n+1):
+            dp[i] = dp[i-1]+2*dp[i-2]
+        MAX = n+1
+    return dp[n]
 
+dp = {0:1, 1:1}
+MAX = 2
 while True:
     try:
         n = int(sys.stdin.readline())
-        print(dp[n])
+        print(solve(n))
     except:
         break
