54c885306d7de95e7621b125b0a00d0fe6685927
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-10 22:02:47 +0900

    [Silver IV] Title: 카드 놓기, Time: 32 ms, Memory: 31632 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.py
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -1,4 +1,3 @@
 import itertools as i
-r=range
-n,k=[int(input())for _ in r(2)]
-print(len(set(map("".join,i.permutations((input()for _ in r(n)),k)))))
+k,*a=[input()for _ in range(int(input())+1)]
+print(len(set(map("".join,i.permutations(a,int(k))))))
