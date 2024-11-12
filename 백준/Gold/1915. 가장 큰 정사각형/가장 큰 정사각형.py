f7d934e55287979cb889ca17e60f566a159ede63
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-12 14:05:33 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 716 ms, Memory: 56408 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -3,6 +3,5 @@ n,m=map(int,input().split())
 d=[list(map(int,input()))for _ in r(n)]
 for i in r(1,n):
     for j in r(1,m):
-        if d[i][j]:
-            d[i][j]+=min(d[i-1][j],d[i][j-1],d[i-1][j-1])
+        d[i][j]+=min(d[i-1][j],d[i][j-1],d[i-1][j-1])*d[i][j]
 print(max(map(max,d))**2)
