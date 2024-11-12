2c005b1a52bc07b31f841d1e60620dfff5f6c37c
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-12 21:17:57 +0900

    [Silver II] Title: 민균이의 계략, Time: 36 ms, Memory: 33316 KB -BaekjoonHub

--- 백준/Silver/11568. 민균이의 계략/민균이의 계략.py
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.py
@@ -1,11 +1,9 @@
+import bisect as f
 (N,),a=[[*map(int,i.split())] for i in open(0)]
 t=[a[0]]
-for i,b in enumerate(a[1:]):
+for b in a[1:]:
     if t[-1]<b:
         t.append(b)
     else:
-        for j,c in enumerate(t):
-            if c>=b:
-                t[j]=b
-                break
+        t[f.bisect_left(t,b)]=b
 print(len(t))
