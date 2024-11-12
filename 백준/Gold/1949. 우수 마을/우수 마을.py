2926c0d85958149dcc07190b3efe1ff855b5922d
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-13 00:33:37 +0900

    [Gold II] Title: 우수 마을, Time: 64 ms, Memory: 37724 KB -BaekjoonHub

--- 백준/Gold/1949. 우수 마을/우수 마을.py
+++ 백준/Gold/1949. 우수 마을/우수 마을.py
@@ -3,7 +3,7 @@ sys.setrecursionlimit(9**6)
 def f(c):
     for d in q[c]:
         if p[d]:p[d]=0;f(d);w[c][0]+=w[d][1];w[c][1]+=max(w[d])
-(N,),p,*s=[[*map(int,i.split())]for i in open(0)]
+N,p,*s=[[*map(int,i.split())]for i in open(0)]
 q,w=zip(*[[[],[t,0]]for t in p])
 for A,B in s:q[A-1].append(B-1);q[B-1].append(A-1)
 p[0]=0;f(0)
