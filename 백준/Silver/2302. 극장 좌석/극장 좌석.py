c756af982b71bf75052fe3ca5c6a3959214b5b95
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-12 15:30:44 +0900

    [Silver I] Title: 극장 좌석, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.py
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.py
@@ -1,11 +1,9 @@
-N,M=int(input()),int(input())
+N,M,*a=map(int,open(0))
+a=[0]+a+[N+1]
+d=1
 f={0:1,1:1}
-for i in range(2,42):
+for i in range(2,41):
     f[i]=f[i-1]+f[i-2]
-arr=[int(input()) for _ in range(M)]+[N+1]
-l=0
-dp=1
-for a in arr:
-    dp*=f[a-l-1]
-    l=a
-print(dp)
+for x,y in zip(a,a[1:]):
+    d*=f[y-x-1]
+print(d)
