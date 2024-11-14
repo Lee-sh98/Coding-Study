829cd6e6737604ecf574aec1bb915ef68958f610
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-14 19:26:03 +0900

    [Silver II] Title: 최대 페이지 수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.py
+++ 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.py
@@ -1,5 +1,4 @@
 import sys
-from heapq import *
 input = sys.stdin.readline
 
 N, M = map(int,input().split())
@@ -9,7 +8,6 @@ dp = [[0]*(N+1) for _ in range(M+1)]
 for i in range(1, M+1):
     for j in range(1, N+1):
         day, page = chapters[i-1]
-
         if j < day:
             dp[i][j] = dp[i-1][j]
         else:
