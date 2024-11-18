a240da5305bf6eae70c803ba09d529e13b25e90a
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-18 16:24:33 +0900

    [Bronze II] Title: Sum Squared Digits Function, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/15296. Sum Squared Digits Function/Sum Squared Digits Function.py
+++ 백준/Bronze/15296. Sum Squared Digits Function/Sum Squared Digits Function.py
@@ -1,7 +1,4 @@
-(P,),*Q=[[*map(int,i.split())]for i in open(0)]
-for K,b,n in Q:
+for K,b,n in [[*map(int,i.split())]for i in open(0)][1:]:
     t=0
-    while n:
-        n,r=divmod(n,b)
-        t+=r**2
+    while n:n,r=divmod(n,b);t+=r**2
     print(K,t)
