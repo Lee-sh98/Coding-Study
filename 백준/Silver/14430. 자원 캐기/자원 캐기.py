53cddb4d4fcaf985fef7517658f4bc4b9f58e1be
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-19 17:28:54 +0900

    [Silver II] Title: 자원 캐기, Time: 84 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/14430. 자원 캐기/자원 캐기.py
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.py
@@ -1,14 +1,6 @@
 N,M=map(int, input().split())
-a=[[*map(int, input().split())] for _ in range(N)]
-for i in range(N):
-    for j in range(M):
-        if i==0 and j==0:
-            continue
-        elif i==0:
-            a[i][j]+=a[i][j-1]
-        elif j==0:
-            a[i][j]+=a[i-1][j]
-        else:
-            a[i][j]+=max(a[i][j-1],a[i-1][j])
-
-print(a[N-1][M-1])
+a=[[0]*(M+1)]+[[0]+[*map(int, input().split())] for _ in range(N)]
+for i in range(1,N+1):
+    for j in range(1,M+1):
+        a[i][j]+=max(a[i][j-1],a[i-1][j])
+print(a[N][M])
