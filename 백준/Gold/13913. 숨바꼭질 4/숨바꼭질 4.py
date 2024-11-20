698e59c89f4183e70389d42e9acbf07bf46479c5
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-20 17:23:23 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 188 ms, Memory: 41964 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -1,8 +1,7 @@
 N,K=map(int,input().split())
 q,w,v,x=[N],[M:=100001]*M,[s:=0]*M,[]
-w[N]=N
 while s<len(q) and (c:=q[s])!=K:
-    c=q[s];s+=1
+    s+=1
     for d in(c+1,c-1,2*c):
         if 0<=d<M and w[d]==M:v[d]=v[c]+1;w[d]=c;q+=[d]
 print(v[K])
