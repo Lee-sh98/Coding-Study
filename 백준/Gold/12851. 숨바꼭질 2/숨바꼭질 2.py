88057d37af226b9b6c15ad8b0ee679fb79c0161b
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-20 16:24:47 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 156 ms, Memory: 135844 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,10 +1,8 @@
-import collections as w
-M=100001
 N,K=map(int,input().split())
-q,v=w.deque([N]),[M]*M;v[N]=r=0
-while q:
-    c=q.popleft();r+=1*(c==K)
+q,v=[N],[M:=100001]*M;v[N]=r=s=0
+while s<len(q):
+    c=q[s];s+=1;r+=c==K
     for d in(c+1,c-1,2*c):
-        if 0<=d<M and v[c]<v[d]:v[d]=v[c]+1;q.append(d)
+        if 0<=d<M and v[c]<v[d]:v[d]=v[c]+1;q+=[d]
 print(v[K])
 print(r)
