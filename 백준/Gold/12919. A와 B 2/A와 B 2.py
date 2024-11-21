ff358cbca7a51c265c5220ad017e65d703db9b18
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-21 19:32:33 +0900

    [Gold V] Title: A와 B 2, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.py
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -1,9 +1,7 @@
 def f(x):
     if not x:return 0
     if len(x)==len(S):return x==S
-    a=b=0
-    if x[-1]=='A':a=f(x[:-1])
-    if x[0]=='B':b=f(x[1:][::-1])
+    a=x[-1]=='A'and f(x[:-1]);b=x[0]=='B'and f(x[1:][::-1])
     return a or b
 
 S,T=input(),input()
