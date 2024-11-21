83ca70fe12b41d27adad434ec29225292ced4ce9
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-21 16:52:21 +0900

    [Bronze II] Title: 가장 많은 글자, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.py
+++ 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.py
@@ -1,3 +1,3 @@
-a={chr(i):0 for i in range(97,123)}
-for c in filter(str.isalpha,open(0).read()):a[c]+=1
-print(*filter(lambda x:a[x]==max(a.values()),a),sep="")
+a={}
+for c in open(0).read():a[c]=a.get(c,0)+c.isalpha()
+print(*sorted(x for x in a if a[x]==max(a.values())),sep="")
