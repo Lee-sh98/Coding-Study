ff4091336597c51050f6ca5d25543d3fec05d86e
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-21 20:52:17 +0900

    [Gold V] Title: 0 만들기, Time: 112 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.py
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -1,6 +1,7 @@
 def f(s,d):
     if d==N:
-        if eval(s.replace(' ',''))==0:r.append(s)
+        if eval(s.replace(' ',''))==0:print(s)
     else:
         for i in" +-":f(s+i+str(d+1),d+1)
-for _ in" "*int(input()):N=int(input());r=[];f("1",1);print(*r,"",sep="\n")
+T,*M=map(int,open(0))
+for N in M:f("1",1);print()
