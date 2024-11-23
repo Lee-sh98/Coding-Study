63ad09e370b5d8cae1c1c3d9b7df86f2dc4120a9
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-11-23 14:19:57 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
@@ -1,4 +1,4 @@
 N,K=map(int,input().split())
-s=[1]*1001
+s=[1]*-~N
 for i in range(2,N+1):
-    for j in range(i,N+1,i):K-=s[j];s[j]=0;not K and exit(print(j))
+ for j in range(i,N+1,i):K-=s[j];s[j]=0;not K and exit(print(j))
