4ccdacb2f02e07370d57727ccbd454a18998ded2
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-12-10 23:18:31 +0900

    [Bronze I] Title: 콘서트, Time: 372 ms, Memory: 129196 KB -BaekjoonHub

--- 백준/Bronze/16466. 콘서트/콘서트.py
+++ 백준/Bronze/16466. 콘서트/콘서트.py
@@ -1,10 +1,5 @@
 N=int(input())
-a,*s=sorted(map(int,input().split()))
-if a==1:
-    i=0
-    while i<N-1 and a+1==s[i]:
-        a=s[i]
-        i+=1
-    print(a+1)
-else:
-    print(1)
+s=[0]+sorted(map(int,input().split()))
+i=0
+while i<N and s[i]+1==s[i+1]:i+=1
+print(s[i]+1)
