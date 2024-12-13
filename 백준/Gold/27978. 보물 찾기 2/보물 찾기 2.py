d426853f56ec52ded3b5631670a1039862e29fea
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-12-13 14:17:40 +0900

    [Gold III] Title: 보물 찾기 2, Time: 500 ms, Memory: 118436 KB -BaekjoonHub

--- 백준/Gold/27978. 보물 찾기 2/보물 찾기 2.py
+++ 백준/Gold/27978. 보물 찾기 2/보물 찾기 2.py
@@ -1,37 +1,22 @@
-import sys
 from heapq import *
-input = sys.stdin.readline
-INF = int(1e9)
+R=range
+F = int(1e9)
 
-H, W = map(int, input().split())
-tx, ty = 0,0
-sea = [input().rstrip() for _ in range(H)]
-directions = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
-distances = [[INF]*W for _ in range(H)]
-q = []
-
-for i in range(H):
-    for j in range(W):
-        if sea[i][j] == 'K':
-            heappush(q, (0, i, j))
-            distances[i][j] = 0
-        elif sea[i][j] == '*':
-            tx, ty = i, j
-
-while q:
-    dist, x, y = heappop(q)
-    if x==tx and y==ty:
-        break
-    if distances[x][y] < dist:
-        continue
-    for dx, dy in directions:
-        nx, ny = x+dx, y+dy
-        ndist = dist+(dy!=1)
-        if not(0<=nx<H and 0<=ny<W) or sea[nx][ny] == '#':
-            continue
-        if ndist<distances[nx][ny]:
-            distances[nx][ny] = ndist
-            heappush(q, (ndist, nx, ny))
-
-if distances[tx][ty]==INF:distances[tx][ty]=-1
-print(distances[tx][ty])
+f,*s=[*open(0)]
+H,W=map(int,f.split())
+S,T=0,0
+e=[(i,j)for j in R(-1,2)for i in R(-1,2)if not(i==j==0)]
+d=[[F]*W for _ in R(H)]
+Q=[]
+for i in R(H):
+    for j in R(W):
+        if s[i][j]=='K':heappush(Q,(0,i,j));d[i][j]=0
+        if s[i][j]=='*':S,T=i,j
+while Q:
+    c,x,y=heappop(Q)
+    if d[x][y]<c:continue
+    for u,v in e:
+        p,q,r=x+u,y+v,c+(v<1)
+        if 0<=p<H and 0<=q<W and s[p][q]!='#'and r<d[p][q]:d[p][q]=r;heappush(Q,(r,p,q))
+if d[S][T]==F:d[S][T]=-1
+print(d[S][T])
