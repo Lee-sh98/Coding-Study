3ffe87992979db4d85c597183e24094097bfd282
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-12-13 15:07:21 +0900

    [Gold III] Title: 보물 찾기 2, Time: 464 ms, Memory: 118148 KB -BaekjoonHub

--- 백준/Gold/27978. 보물 찾기 2/보물 찾기 2.py
+++ 백준/Gold/27978. 보물 찾기 2/보물 찾기 2.py
@@ -1,17 +1,14 @@
-from heapq import *
-R,F=range,9**9
+from heapq import*
 f,*s=[*open(0)]
 H,W=map(int,f.split())
-e=[(i,j)for j in R(-1,2)for i in R(-1,2)if not(i==j==0)]
-d=[W*[F]for _ in R(H)]
-for x in R(H*W):
- i,j=x//W,x%W
- if'K'==s[i][j]:Q=[(0,i,j)];d[i][j]=0
+d=[W*[9**9]for _ in range(H)]
+Q=[]
+for x in range(H*W):Q+=[(0,x//W,x%W)]*('K'==s[x//W][x%W])
 while Q:
  c,x,y=heappop(Q)
  if'*'==s[x][y]:exit(print(c))
  if d[x][y]<c:continue
- for u,v in e:
+ for u,v in zip([-1,-1,-1,0,0,1,1,1],[-1,0,1,-1,1,-1,0,1]):
   p,q,r=x+u,y+v,c+(v<1)
   if 0<=p<H and 0<=q<W and'#'!=s[p][q]and r<d[p][q]:d[p][q]=r;heappush(Q,(r,p,q))
 print(-1)
