38f04fcf4b629cae9dd48b3a67ebef379474ecab
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-12-13 14:30:52 +0900

    [Gold III] Title: 보물 찾기 2, Time: 488 ms, Memory: 118332 KB -BaekjoonHub

--- 백준/Gold/27978. 보물 찾기 2/보물 찾기 2.py
+++ 백준/Gold/27978. 보물 찾기 2/보물 찾기 2.py
@@ -1,22 +1,18 @@
 from heapq import *
-R=range
-F = int(1e9)
-
+R,F=range,10**9
 f,*s=[*open(0)]
 H,W=map(int,f.split())
-S,T=0,0
 e=[(i,j)for j in R(-1,2)for i in R(-1,2)if not(i==j==0)]
-d=[[F]*W for _ in R(H)]
-Q=[]
-for i in R(H):
-    for j in R(W):
-        if s[i][j]=='K':heappush(Q,(0,i,j));d[i][j]=0
-        if s[i][j]=='*':S,T=i,j
+d=[W*[F]for _ in R(H)]
+for x in R(H*W):
+    i,j=x//W,x%W
+    if'K'==s[i][j]:Q=[(0,i,j)];d[i][j]=0
+    if'*'==s[i][j]:S,T=i,j
 while Q:
     c,x,y=heappop(Q)
     if d[x][y]<c:continue
     for u,v in e:
         p,q,r=x+u,y+v,c+(v<1)
-        if 0<=p<H and 0<=q<W and s[p][q]!='#'and r<d[p][q]:d[p][q]=r;heappush(Q,(r,p,q))
+        if 0<=p<H and 0<=q<W and'#'!=s[p][q]and r<d[p][q]:d[p][q]=r;heappush(Q,(r,p,q))
 if d[S][T]==F:d[S][T]=-1
 print(d[S][T])
