cc3582225a20d6a6b85df118e401954d4f02e343
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-12-15 22:41:14 +0900

    [Bronze I] Title: 생일 맞추기, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/28358. 생일 맞추기/생일 맞추기.py
+++ 백준/Bronze/28358. 생일 맞추기/생일 맞추기.py
@@ -1,10 +1,6 @@
-T=int(input())
-month=[31,30]*3+[30,31]*3
-month[1]=29
-month[6]=31
-
-for _ in range(T):
-    a=input().replace(" ","")
+month=[31,29]+[31,30,31,30,31]*2
+for a in[*open(0)][1:]:
+    a=a.replace(" ","")
     b=[i for i in range(1,13) if a[i%10]=="0" and (i<10 or (9<i and a[1]=="0"))]
     c=[*filter(lambda i: a[i%10]=="0" and (i<10 or (9<i and a[i//10]=="0")),range(1,32))]
     d=sum(sum(map(month[i-1].__ge__, c)) for i in b)
