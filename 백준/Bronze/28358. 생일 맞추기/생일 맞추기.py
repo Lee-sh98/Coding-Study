b8b06b0dd0a993c8f418dff9b69e80ec4ad10169
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2024-12-15 22:38:43 +0900

    [Bronze I] Title: 생일 맞추기, Time: 120 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/28358. 생일 맞추기/생일 맞추기.py
+++ 백준/Bronze/28358. 생일 맞추기/생일 맞추기.py
@@ -1,17 +1,11 @@
 T=int(input())
-month=[0]+[31,30]*3+[30,31]*3
-month[2]=29
-month[7]=31
+month=[31,30]*3+[30,31]*3
+month[1]=29
+month[6]=31
 
 for _ in range(T):
-    a=[*input().split()]
-    b={str(i) for i in range(10) if a[i]=="0"}
-
-    c={int(x+y) for y in b for x in b if int(x+y)<13}
-    d={int(x+y) for y in b for x in b if int(x+y)<32}
-
-    e={*map(int,(b|c)-{"0",0})}
-    f={*map(int,(b|d)-{"0",0})}
-    g=sum(sum(map(month[x].__ge__,f)) for x in e)
-
-    print(g)
+    a=input().replace(" ","")
+    b=[i for i in range(1,13) if a[i%10]=="0" and (i<10 or (9<i and a[1]=="0"))]
+    c=[*filter(lambda i: a[i%10]=="0" and (i<10 or (9<i and a[i//10]=="0")),range(1,32))]
+    d=sum(sum(map(month[i-1].__ge__, c)) for i in b)
+    print(d)
