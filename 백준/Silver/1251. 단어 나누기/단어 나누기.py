c337ca34ffb0e5ed4da0541baa54dd3d399eb983
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2025-01-11 14:57:39 +0900

    [Silver V] Title: 단어 나누기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,5 +1,2 @@
 s=input()
-r=[]
-for i in range(1,len(s)):
-    for j in range(i+1,len(s)):r+=[s[:i][::-1]+s[i:j][::-1]+s[j:][::-1]]
-print(min(r))
+print(min(s[:i][::-1]+s[i:j][::-1]+s[j:][::-1]for i in range(1,len(s))for j in range(i+1,len(s))))
