3d274b97b58bcbc2871991d3a875c3507d82a3a2
이승훈 <55966515+Lee-sh98@users.noreply.github.com>
2025-02-14 00:10:47 +0900

    [Bronze III] Title: 모험의 시작, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/31789. 모험의 시작/모험의 시작.py
+++ 백준/Bronze/31789. 모험의 시작/모험의 시작.py
@@ -1,7 +1,2 @@
-N=int(input())
-X,S=map(int, input().split())
-r=0
-for _ in range(N):
-    c,p=map(int, input().split())
-    if c<=X:r=max(r,p)
-print("YNEOS"[r<=S::2])
+(N),(X,S),*t=[map(int,i.split())for i in open(0)]
+print("YNEOS"[max((p for c,p in t if c<=X),default=0)<=S::2])
